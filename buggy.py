a37a7a2eac92949250d9431088812b9d5c029981
Faisal <mdfaisal.2k5@gmail.com>
2026-05-08 02:57:42 +0530

    Auto-fix: Added parentheses to ensure correct operator precedence.

--- buggy.py
+++ buggy.py
@@ -1,5 +1,3 @@
-{"def calculate_discount(price, discount_percentage):
-    # Original line: Incorrect syntax for calculating discount amount. The '*' operator has a higher precedence than '/'.
-    discount_amount = (price / 100) * discount_percentage
-    return discount_amount
-}"
+def calculate_discount(price, discount_percentage):
+discount_amount = ((price / 100) * discount_percentage)
+return discount_amount
